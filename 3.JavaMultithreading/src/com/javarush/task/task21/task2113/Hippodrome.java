c58809c004d17d4a72eead0eed6c28bdd82a4a2d
Artem Puponin <artem74@gmail.com>
2017-03-22 09:24:19 +0300

    Initial commit

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Me on 15.03.2017.
+ */
+public class Hippodrome {
+
+
+
+    public   ArrayList<Horse> horses = new ArrayList<>();
+    public static Hippodrome game;
+
+    public static void main (String[] args) throws InterruptedException{
+        game = new Hippodrome();
+        Horse pegas = new Horse("Pegas",3,0);
+        Horse ralph = new Horse("Ralph",3,0);
+        Horse ronald = new Horse("Ronald",3,0);
+        game.getHorses().add(pegas);
+        game.getHorses().add(ralph);
+        game.getHorses().add(ronald);
+    }
+
+
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
+
+
+    public void run() throws InterruptedException {
+        for (int i = 1; i <= 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+
+    public void move() {
+
+
+    }
+
+    public void print() {
+
+    }
+
+
+
+
+
+}
